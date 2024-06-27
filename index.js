7e7ec70de0ba29f3d718fccbf5195457c442f82e
Brandon VanFossen <bgvfossen@gmail.com>
2024-06-27 15:15:50 -0400

    added api routes

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ const express = require('express')
 const path = require('path')
 
 const port = process.env.PORT
-app.use(express.static(path.join(__dirname, "apps", 'public' )))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "apps", 'public', 'index.html'))   
+app = express()
+
+app.use(express.static(path.join(__dirname, "apps", "HighLevelAppBoilerplate",  'public' )))
+
+app.get('/', (req, res) => {
+     res.sendFile(path.join(__dirname, "apps","HighLevelAppBoilerplate",  'public', 'index.html'))
+
 })
 
+app.get('/code', (req, res) => {
+    console.log(req.query.code)
+    res.send('data sent')
+})
+    
+
+app.get('/url', (req, res) => {
+    res.json({url: process.env.URL})
+})
+
+app.get('/subscribed', (req, res) =>{
+    res.send('subscribed')
+})
 
 app.listen(port, console.log("server running on " + port))
